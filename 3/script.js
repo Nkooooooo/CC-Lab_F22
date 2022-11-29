1645110662e1e133879cbf25e2445ff0857b00e3
Nkooooooo <119025852+Nkooooooo@users.noreply.github.com>
2022-11-29 18:34:26 +0800

    1129

--- 3/script.js
+++ 3/script.js
@@ -3,7 +3,12 @@ console.log("Test")
 function buttonClicked() {
     // alert("Clicked")
     background(random(255), random(255), random(255))
-} let angleDeg = 0;
+
+    radDist = 0;
+    console.log(x);
+}
+
+let angleDeg = 0;
 let x, y;
 let dia = 0;
 let radDist = 0;
@@ -16,9 +21,12 @@ function setup() {
     canvas.parent("canvasContainer")
     background(153, 204, 255);
     frameRate(10);
-
 }
 
+// function mouseIsPressed() {
+//     x = windowWidth / 2;
+//     y = windowHeight / 2;
+// }
 function draw() {
 
     angleDeg = angleDeg + 20;
@@ -28,18 +36,11 @@ function draw() {
     let cosVal = cos(angleRad) * radDist;
     let sinVal = sin(angleRad) * radDist;
 
-    // let r = map(mouseX, windowWidth, windowHeight, 0, 255);
-    // let g = map(mouseX, windowWidth, windowHeight, 255, 255);
-    // let b = map(mouseX, windowWidth, windowHeight, 0, 0);
-
-
     let modulo = frameCount % 80;
-    console.log(modulo);
 
     let r = 252;
     let g = map(modulo, 0, 80, 98, 252);
     let b = 3;
-    // console.log( g);
 
 
     dia = dia + 0.5;
@@ -48,9 +49,14 @@ function draw() {
     x = windowWidth / 2 + cosVal;
     y = windowHeight / 2 + sinVal;
 
+    // if mouseIsPressed(){
+    //     x = windowWidth / 2;
+    //     y = windowHeight / 2;
+    // }
+    circle(x, y, modulo);
     noStroke();
     fill(r, g, b);
-    circle(x, y, modulo);
+
 
     radDist = radDist + 1;
 }
